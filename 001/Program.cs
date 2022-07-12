7812e73a7b27e833c1d92acadb14fdab0c673e86
Roman <igoshinr.estho@gmail.com>
2022-07-12 21:41:29 +0300

    001

---
+++ 001/Program.cs
@@ -0,0 +1,18 @@
+﻿int a, b;
+
+
+
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+bool what;
+
+if (b * b == a || a * a == b)
+{
+    what = true;
+}
+else
+{
+    what = false;
+}
+
+Console.WriteLine(what);
