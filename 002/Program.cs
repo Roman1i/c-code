a08a3b6d8317ff5df054ce21af6d1583b4cf2539
Roman <igoshinr.estho@gmail.com>
2022-07-12 21:52:46 +0300

    002.1

--- 002/Program.cs
+++ 002/Program.cs
@@ -1,18 +1,25 @@
 ﻿int n;
 n = Convert.ToInt32(Console.ReadLine());
-if (n == 1)
-    Console.WriteLine("Понедельник");
-if (n == 2)
-    Console.WriteLine("Вторник");
-if (n == 3)
-    Console.WriteLine("Среда");
-if (n == 4)
-    Console.WriteLine("Четверг");
-if (n == 5)
-    Console.WriteLine("Пятница");
-if (n == 6)
-    Console.WriteLine("Суббота");
-if (n == 7)
-    Console.WriteLine("Воскресенье");
 
+if (n >= 1 && n <= 7)
+{
+    if (n == 1)
+        Console.WriteLine("Понедельник");
+    if (n == 2)
+        Console.WriteLine("Вторник");
+    if (n == 3)
+        Console.WriteLine("Среда");
+    if (n == 4)
+        Console.WriteLine("Четверг");
+    if (n == 5)
+        Console.WriteLine("Пятница");
+    if (n == 6)
+        Console.WriteLine("Суббота");
+    if (n == 7)
+        Console.WriteLine("Воскресенье");
+}
+else 
+{
+    Console.WriteLine("нет");
+}
 
